84af3cee068d1008665c45b837d38df2932f8fa3
truephelle@yandex.ru <truephelle@yandex.ru>
2025-12-21 23:26:04 +0300

    really fixed a last line

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,25 @@
 import { defineConfig } from "eslint/config";
+import tsPlugin from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
 
 export default defineConfig([
 	{
 		ignores: ["/build", "**/*.min.js", "/node_modules "],
 	},
+	{
+		files: ["**/*.ts", "**/*.tsx"],
+		languageOptions: {
+			parser: tsParser,
+			parserOptions: {
+				ecmaVersion: "latest",
+				sourceType: "module",
+			},
+		},
+		plugins: {
+			"@typescript-eslint": tsPlugin,
+		},
+		rules: {
+			"eol-last": ["error", "always"]
+		}
+	}
 ]);
